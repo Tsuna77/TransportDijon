d3117c25f11599a916acd60e3d6869264937b5cb
Darklewis <darklewis@elektralife.fr>
2018-11-02 11:06:01 +0100

    Retrait du menu apparaissant dans la fenêtre About
    Ticket #9

--- app/src/main/java/fr/tsuna/transportdijon/AboutActivity.java
+++ app/src/main/java/fr/tsuna/transportdijon/AboutActivity.java
@@ -63,7 +63,7 @@ public class AboutActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.about, menu);
+		//getMenuInflater().inflate(R.menu.about, menu);
 		return true;
 	}
 
